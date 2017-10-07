95a3c3b7b937b6bbe22e3d4f1a8cedd0c03a4eee
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-10-07 22:51:17 +0300

    add v2; rename arguments

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ var a = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1],
  e = [1,NaN,3,5,-3];
 
 
-function res(date){
-	var arr = date.filter(function(n){ return( !isNaN(n) ) });
+function res(data){
+	var arr = data.filter(function(n){ return( !isNaN(n) ) });
 	var max = Math.max.apply(Math,arr);
 	var min = Math.min.apply(Math,arr);
 	var sum =  Object.values(arr).reduce((a, b) => a + b);
@@ -30,3 +30,39 @@ console.table(res(b));
 console.table(res(c));
 console.table(res(d));
 console.table(res(e));
+
+// v2 
+console.info("v2");
+
+var a_v2 = [3,"1112",0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1,"1112"],
+ b_v2 = [-1,-8,-2,"1112"],
+ c_v2 = [1,7,3],
+ d_v2 = [1,undefined,3,5,-3],
+ e_v2 = [1,"1112",NaN,"1112",3,5,-3];
+
+function res_v2(data){
+
+	var max = Number.NEGATIVE_INFINITY ;
+	var min= Number.POSITIVE_INFINITY ;
+	var sum = 0;
+  
+	for(var i = 0; i < data.length; i++){
+	  if(Number.isInteger(data[i])){
+	  	if(max < data[i]) max = data[i]; 
+	    if(min > data[i]) min = data[i];
+	    sum = sum + data[i];    
+	  }
+	}
+	 return {
+        'max': max,
+        'min': min,
+        'sum': sum
+    }
+
+}
+	
+console.log(res_v2(a_v2));
+console.log(res_v2(b_v2));
+console.log(res_v2(c_v2));
+console.log(res_v2(d_v2));
+console.log(res_v2(e_v2));
